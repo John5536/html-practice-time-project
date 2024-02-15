b7c061aa4e96eb41006ae52eb5f48f247f56a140
John5536 <90068285+John5536@users.noreply.github.com>
2024-02-15 14:53:16 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
 document.addEventListener('DOMContentLoaded', function() {
-    alert("I live");
+ 
 
 });
 
 var time_spent
-var time_left = 3650
+var time_left = 152
 var base_num
+var day_spent = 152
+
 
 document.getElementById("time_button").onclick = function(){
     time_spent = document.getElementById("time_input").value;
     document.getElementById("time_11").textContent = time_spent;
-var new_time = parseInt(document.getElementById("time_11").textContent);
-document.getElementById("time_left_set").textContent = time_left - new_time;
 
+//var new_time = parseInt(document.getElementById("time_11").textContent);
+//document.getElementById("time_left_set").textContent = time_left - new_time;
+
+
+}
+
+document.getElementById("Days_button").onclick = function(){
+    day_spent = document.getElementById("Day_input").value;
+    var time_subtracted = parseInt(document.getElementById("time_left_set").textContent);
+    document.getElementById("time_left_set").textContent = time_subtracted - day_spent;
+
+    
 
 
 
 
 }
-
-
 
 
 
